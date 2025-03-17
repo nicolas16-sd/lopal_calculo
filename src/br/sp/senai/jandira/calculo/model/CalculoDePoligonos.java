54ab522d4b028e0c41a1fff3f0dc0e3bceb14efb
NICOLAS DOS SANTOS DURÃO <25132912@CFP127ALUNO.LOCAL>
2025-03-17 11:17:25 -0300

    novas atualizacoes

---
+++ src/br/sp/senai/jandira/calculo/model/CalculoDePoligonos.java
@@ -0,0 +1,23 @@
+package br.sp.senai.jandira.calculo.model;
+
+import java.util.Scanner;
+
+public class CalculoDePoligonos {
+	
+	public static void calcularRetangulo() {
+		
+		Retangulo retangulo = new Retangulo();
+		Scanner leitor = new Scanner(System.in);
+		
+		System.out.print("Altura do retângulo: ");
+		double altura = leitor.nextDouble();
+	
+		System.out.print("Base do retângulo: ");
+		double base = leitor.nextDouble();
+		
+		retangulo.setAltura(altura);
+		retangulo.setBase(base);
+		retangulo.mostrarDados();
+	}
+	
+}
